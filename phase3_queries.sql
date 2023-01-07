b1a3ba6134a6977aa217a7c0a0d3a61c51c3a674
Hamid Zehtab <hamid13801399@gmail.com>
2023-01-07 21:34:55 +0330

    Update phase3_queries.sql

--- phase3_queries.sql
+++ phase3_queries.sql
@@ -1,6 +1,6 @@
 use project;
 Select * from tbl_buy_doreh where id_username = 1 order by Id desc limit 10 ;
-Select b.title from tbl_doreh a left join tbl_reshteh b on a.id_reshteh = b.Id; 
+Select concat(b.title," Code ",a.Id) as list_doreh from tbl_doreh a left join tbl_reshteh b on a.id_reshteh = b.Id; 
 Select concat(fname," ",lname) as names from tbl_users;
 Select title from tbl_reshteh;
 Select d.Title,concat(b.fname," ",b.lname) as name,a.date_buy from tbl_buy_doreh a left join tbl_users b on a.id_username = b.username left join tbl_doreh c on a.id_doreh = c.Id left join tbl_reshteh d on d.Id = c.id_reshteh ;
