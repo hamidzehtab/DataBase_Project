1368de48c74e6319a5cb076b69e2e00b91a187a6
sajjad arghaee <84894722+Sajjad-Arghaee@users.noreply.github.com>
2023-01-11 12:01:41 +0330

    formatting queries

--- db_project/api/views.py
+++ db_project/api/views.py
@@ -7,7 +7,6 @@ def get_projects(request):
     cursor.execute('SELECT * FROM tbl_cart;')
     projects = cursor.fetchall()
     cursor.execute('''SHOW columns FROM project.tbl_cart;''')
-    result = []
     columns = []
     for column in cursor.fetchall():
         columns.append(column[0])
@@ -66,7 +65,9 @@ def list_of_product_types(request):
 def list_of_orders(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select d.Title,concat(b.fname," ",b.lname) as name,a.date_buy from tbl_buy_doreh a left join tbl_users b on a.id_username = b.username left join tbl_doreh c on a.id_doreh = c.Id left join tbl_reshteh d on d.Id = c.id_reshteh ; ')
+        '''Select d.Title,concat(b.fname," ",b.lname) as name,a.date_buy
+         from tbl_buy_doreh a left join tbl_users b on a.id_username = b.username left join tbl_doreh c on a.id_doreh = c.Id
+          left join tbl_reshteh d on d.Id = c.id_reshteh ; ''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -80,7 +81,9 @@ def list_of_orders(request):
 def list_of_top_10_ussers_all(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id group by a.id_username order by count(*) desc Limit 10 ; ')
+        '''Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name
+         from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id 
+         group by a.id_username order by count(*) desc Limit 10 ; ''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -98,7 +101,9 @@ def change_users(request):
 def list_of_top_10_users_month(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id where a.date_buy >= DATE(NOW() - INTERVAL 1 Month) group by a.id_username order by count(*) desc Limit 10 ;')
+        '''Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name
+         from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id where a.date_buy >= DATE(NOW() - INTERVAL 1 Month)
+          group by a.id_username order by count(*) desc Limit 10 ;''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -112,7 +117,9 @@ def list_of_top_10_users_month(request):
 def list_of_top_10_users_week(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id where a.date_buy >= DATE(NOW() - INTERVAL 7 DAY) group by a.id_username order by count(*) desc Limit 10 ;')
+        '''Select count(*) as num_buy, a.id_username , concat(b.fname," ",b.lname) as name
+         from tbl_buy_doreh a left join tbl_users b on a.id_username = b.Id where a.date_buy >= DATE(NOW() - INTERVAL 7 DAY
+          group by a.id_username order by count(*) desc Limit 10 ;''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -126,7 +133,9 @@ def list_of_top_10_users_week(request):
 def list_of_top_sold_product_week(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select count(*) as times_bought , c.Title from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id left join tbl_reshteh c on c.Id = b.id_reshteh where a.date_buy >= DATE(NOW() - INTERVAL 7 Day) group by a.id_doreh order by count(*) desc;')
+        '''Select count(*) as times_bought , c.Title 
+        from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id left join tbl_reshteh c on c.Id = b.id_reshteh
+         where a.date_buy >= DATE(NOW() - INTERVAL 7 Day) group by a.id_doreh order by count(*) desc;''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -140,7 +149,9 @@ def list_of_top_sold_product_week(request):
 def list_of_top_sold_procut_month(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select count(*) as times_bought , c.Title from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id left join tbl_reshteh c on c.Id = b.id_reshteh where a.date_buy >= DATE(NOW() - INTERVAL 1 Month) group by a.id_doreh order by count(*) desc; ')
+        '''Select count(*) as times_bought , c.Title from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id
+         left join tbl_reshteh c on c.Id = b.id_reshteh where a.date_buy >= DATE(NOW() - INTERVAL 1 Month)
+          group by a.id_doreh order by count(*) desc; ''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -154,7 +165,9 @@ def list_of_top_sold_procut_month(request):
 def list_of_special_offers(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select c.title , a.discount2 as off_percentage from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id left join tbl_reshteh c on c.Id = b.id_reshteh where a.discount2 >= (a.fee*15/100);')
+        '''Select c.title , a.discount2 as off_percentage
+         from tbl_buy_doreh a left join tbl_doreh b on a.id_doreh = b.Id left join tbl_reshteh c on c.Id = b.id_reshteh 
+         where a.discount2 >= (a.fee*15/100);''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -168,7 +181,8 @@ def list_of_special_offers(request):
 def list_of_providersof_product_admin(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select concat(a.Id," name ",c.title," offered by ",b.name) as offerd_by from tbl_doreh a left join tbl_institute b on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title;')
+        '''Select concat(a.Id," name ",c.title," offered by ",b.name) as offerd_by
+         from tbl_doreh a left join tbl_institute b on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title;''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -182,7 +196,9 @@ def list_of_providersof_product_admin(request):
 def list_of_providersof_product_admin_2(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select concat(a.Id," name ",c.title," offered by ",b.name) as offerd_by from tbl_institute b left join tbl_doreh a on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title where c.Id = '';')
+        '''Select concat(a.Id," name ",c.title," offered by ",b.name) as offerd_by
+         from tbl_institute b left join tbl_doreh a on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title
+         where c.Id = '';''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
@@ -196,7 +212,9 @@ def list_of_providersof_product_admin_2(request):
 def list_of_cheapest_providerof_products_admin(request):
     cursor = connection.cursor()
     cursor.execute(
-        'Select concat(a.Id," name ",c.title," offered by ",b.name," for the cheapest price") as offerd_by from tbl_institute b left join tbl_doreh a on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title where c.Id = '' order by a.fee desc LIMIT 1;')
+        '''Select concat(a.Id," name ",c.title," offered by ",b.name," for the cheapest price") as offerd_by
+         from tbl_institute b left join tbl_doreh a on a.Id = b.id_doreh left join tbl_reshteh c on a.id_reshteh = c.Title
+          where c.Id = '' order by a.fee desc LIMIT 1;''')
     projects = cursor.fetchall()
     # cursor.execute('''SHOW columns FROM tbl_doreh;''')
     result = []
