ee581061893f5790636e2935057bd415590a09fc
Hamid Zehtab <hamid13801399@gmail.com>
2023-01-11 12:24:42 +0330

    Update phase3_queries.sql

--- phase3_queries.sql
+++ phase3_queries.sql
@@ -68,7 +68,7 @@ select * from tbl_comments where id_buy_doreh=@v1 order by score desc limit 3;
 select Id,count(*) as buy_number from tbl_buy_doreh group by Id;
 
 # 15th query showing the average sale of store to admin
-select avg(fee),month(date_buy) from tbl_buy_doreh group by month(date_buy);
+select avg(fee),month(date_buy),year(date_buy) from tbl_buy_doreh group by month(date_buy),year(date_buy);
 
 # 16th query providers of a given city
 select * from tbl_users group by city;
