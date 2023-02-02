4d650bf80ece5e6367e4d7a35ab177ed801d4fe4
Hamid Zehtab <hamid13801399@gmail.com>
2023-02-02 18:21:08 +0330

    Update views.py

--- db_project/api/views.py
+++ db_project/api/views.py
@@ -182,7 +182,7 @@ def list_of_top3_best_comments_for_agiven_product(request):
         num = request.POST['input']
     cursor = connection.cursor()
     cursor.execute(
-        f'''select * from tbl_comment where id_doreh={num} order by score limit 3; ''')
+        f'''select * from tbl_comment where id_doreh={num} order by desc score limit 3; ''')
     items = cursor.fetchall()
     cursor.execute('''SHOW columns FROM tbl_comment;''')
     columns = []
@@ -191,9 +191,12 @@ def list_of_top3_best_comments_for_agiven_product(request):
     return render(request, 'clientarea.html', {'columns': columns, 'items': items})
 
 def list_of_top3_worst_comments_for_agiven_product(request):
+    num = 101
+    if request.method == 'POST':
+        num = request.POST['input']
     cursor = connection.cursor()
     cursor.execute(
-        '''select * from tbl_comments where id_buy_doreh=@v1 order by score desc limit 3; ''')
+        f'''select * from tbl_comment where id_doreh={num} order by score limit 2; ''')
     items = cursor.fetchall()
     cursor.execute('''SHOW columns FROM tbl_comment;''')
     columns = []
@@ -202,11 +205,14 @@ def list_of_top3_worst_comments_for_agiven_product(request):
     return render(request, 'clientarea.html', {'columns': columns, 'items': items})
 
 def list_of_sale_for_agiven_product_admin(request):
+    num = 101
+    if request.method == 'POST':
+        num = request.POST['input']
     cursor = connection.cursor()
     cursor.execute(
         '''select Id,count(*) as buy_number from tbl_buy_doreh group by Id; ''')
     items = cursor.fetchall()
-    return render(request, 'clientarea.html', {'columns': ['offerd_by'], 'items': items})
+    return render(request, 'clientarea.html', {'columns': ['Id','sale'], 'items': items})
 
 def list_of_average_sale_for_store_admin(request):
     cursor = connection.cursor()
