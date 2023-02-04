91bfc1361bdc2ca54d39481a66ceb8e01ca0034d
sajjad arghaee <arghaei1380@yahoo.com>
2023-02-04 10:46:43 +0330

    product management

---
+++ db_project/api/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import CustomUsers
+from django.forms import ModelForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserCreationForm, self).__init__(*args, **kwargs)
+        # for field in self.fields:
+        #     self.fields[field].widget.attrs.update(
+        #         {'class': 'input'}
+        #     )
+
+
+class CustomAdminCreationForm(ModelForm):
+    class Meta:
+        model = CustomUsers
+        fields = '__all__'
