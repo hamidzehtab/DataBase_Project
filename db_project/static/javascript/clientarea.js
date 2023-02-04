7cca839b3e17c74d42f0d3304eab0e704ce575c6
zarenima <116112101+zarenima@users.noreply.github.com>
2023-02-04 10:59:00 +0330

    sign up user

--- db_project/static/javascript/clientarea.js
+++ db_project/static/javascript/clientarea.js
@@ -193,5 +193,19 @@ function show(vr,b=1) {
 
 
 
-
-    
+    function back1() {
+        state = 1;
+        $('.forms4').slideDown();
+        $('.forms1').fadeOut();
+        hide();
+        alertBox.classList.add("hidden");
+        $('input[type=text]').val("");
+    }
+    function back4() {
+        state = 1;
+        $('.forms4').slideDown();
+        $('.forms1').fadeOut();
+        hide();
+        alertBox.classList.add("hidden");
+        $('input[type=text]').val("");
+    }
